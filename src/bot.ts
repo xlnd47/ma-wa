46b5c573869f1cf4be939ed65834fdb1ade39863
Axel De Schuijmer <axel.deschuijmer@gmail.com>
2021-03-18 20:14:57 +0100

    try again

--- src/bot.ts
+++ src/bot.ts
@@ -2,9 +2,11 @@ require("dotenv").config();
 import { registerCommands, registerEvents } from "./utils/registry";
 import DiscordClient from "./client/client";
 const client = new DiscordClient({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
+import * as mongoose from 'mongoose';
 
 (async () => {
-	client.prefix = process.env.DISCORD_BOT_PREFIX;
+	client.prefix = process.env.DISCORD_BOT_PREFIX || '?';
+	await mongoose.connect(process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "mawa-dev" })
 	await registerCommands(client, "../commands");
 	await registerEvents(client, "../events");
 	await client.login(process.env.DISCORD_BOT_TOKEN);
